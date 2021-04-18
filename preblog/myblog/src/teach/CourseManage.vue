84a25ab0f8d0ba5fcbb07ca15a55f994cf35b438
wsx <1162532214@qq.com>
2021-04-18 14:01:37 +0800

    1

--- preblog/myblog/src/teach/CourseManage.vue
+++ preblog/myblog/src/teach/CourseManage.vue
@@ -1,13 +1,32 @@
 <template>
 
+<div>
+  <el-input
+      class="search-input"
+      size="small"
+      placeholder="请输入查询关键字"
+      prefix-icon="el-icon-search"
+      v-model="search">
+  </el-input>
+  <el-button type="primary" icon="el-icon-search" size="small">搜索</el-button>
+</div>
 </template>
 
 <script>
-export default {
-name: "CourseManage"
-}
+  export default {
+    name: "CourseManage",
+    data(){
+      return{
+        search:''
+      }
+    }
+  }
+
 </script>
 
 <style scoped>
-
+  .search-input{
+    width: 200px;
+    margin-right: 8px;
+  }
 </style>
--- preblog/myblog/src/views/Test1.vue
+++ preblog/myblog/src/views/Test1.vue
@@ -1,26 +1,32 @@
 <template>
   <div>
-    <el-tabs type="border-card" v-model="activeName">
-      <el-tab-pane label="课程管理" name="one">课程管理</el-tab-pane>
-      <el-tab-pane label="教师管理" name="two">教师管理</el-tab-pane>
-      <el-tab-pane label="学院管理" name="third">学院管理</el-tab-pane>
+    <el-tabs  type="card" v-model="activeName" >
+      <el-tab-pane label="课程管理" name="CourseManage"><CourseManage></CourseManage></el-tab-pane>
+      <el-tab-pane label="教师管理" name="TeacherManage"><TeacherManage></TeacherManage></el-tab-pane>
+      <el-tab-pane label="学院管理" name="AcademyManage"><AcademyManage></AcademyManage></el-tab-pane>
     </el-tabs>
   </div>
 
 </template>
 <script>
+
 import CourseManage from "@/teach/CourseManage";
+import AcademyManage from "@/teach/AcademyManage";
+import TeacherManage from "@/teach/TeacherManage";
 export default {
+
   name: "基本信息管理",
   data() {
     return{
-      activeName:"one"
+      activeName: 'CourseManage'
     }
+  },
+  components:{
+    CourseManage,
+    AcademyManage,
+    TeacherManage
   }
 }
-</script>
-<script>
-
 </script>
 
 <style scoped>
